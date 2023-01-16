2714449e5eefb7499d1ed5fd73841819f7cbbb2d
ZeoNasA <108832831+ZeoNasA@users.noreply.github.com>
2023-01-16 11:41:43 +0800

    Add files via upload

---
+++ datapelajar.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2023 at 01:17 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `datapelajar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_pelajar`
+--
+
+CREATE TABLE `contact_pelajar` (
+  `no_ndp` int(9) NOT NULL,
+  `emel` varchar(300) NOT NULL,
+  `alamat` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `info_pelajar`
+--
+
+CREATE TABLE `info_pelajar` (
+  `id` int(3) NOT NULL,
+  `no_ndp` int(9) NOT NULL,
+  `nama_pelajar` varchar(50) NOT NULL,
+  `no_kp` int(50) NOT NULL,
+  `jantina` varchar(50) NOT NULL,
+  `no_hp` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_pelajar`
+--
+ALTER TABLE `contact_pelajar`
+  ADD PRIMARY KEY (`no_ndp`);
+
+--
+-- Indexes for table `info_pelajar`
+--
+ALTER TABLE `info_pelajar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_pelajar`
+--
+ALTER TABLE `contact_pelajar`
+  MODIFY `no_ndp` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23221148;
+
+--
+-- AUTO_INCREMENT for table `info_pelajar`
+--
+ALTER TABLE `info_pelajar`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
